c5ea30ee13122ae3709f5abe8533d26b83d3a909
anewsdev <anewsdev@gmail.com>
2024-09-30 14:28:07 +0900

    init commit

---
+++ src/test/java/com/sparta/nbcspringmemo/NbcSpringMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcspringmemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcSpringMemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
