6d02ed5e95e3567e08cf634b5b29e32dd56e686b
anewsdev <anewsdev@gmail.com>
2024-09-30 15:48:38 +0900

    update, delete 구현

--- src/main/java/com/sparta/nbcspringmemo/controller/MemoController.java
+++ src/main/java/com/sparta/nbcspringmemo/controller/MemoController.java
@@ -41,4 +41,32 @@ public class MemoController {
                 .map(MemoResponseDto::new).toList();
         return responseList;
     }
+
+    @PutMapping("/memos/{id}")
+    public long updateMemo(@PathVariable long id, @RequestBody MemoRequestDto requestDto) {
+        //해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            //해당 메모 가져오기
+            Memo memo = memoList.get(id);
+
+            //memo 수정
+            memo.update(requestDto);
+            return memo.getId();
+
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public long deleteMemo(@PathVariable long id) {
+        //해당 메모가 db에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            memoList.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
 }
