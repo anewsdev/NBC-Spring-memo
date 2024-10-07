69b22415a6a34e299e2270d7e2ccdba21811a4f7
anewsdev <anewsdev@gmail.com>
2024-10-07 20:33:34 +0900

    스프링 부트의 jpa

--- src/main/java/com/sparta/nbcspringmemo/entity/Memo.java
+++ src/main/java/com/sparta/nbcspringmemo/entity/Memo.java
@@ -1,17 +1,24 @@
 package com.sparta.nbcspringmemo.entity;
 
 import com.sparta.nbcspringmemo.dto.MemoRequestDto;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity // JPA가 관리할 수 있는 Entity 클래스 지정
 @Getter
 @Setter
+@Table(name = "memo") // 매핑할 테이블의 이름을 지정
 @NoArgsConstructor
 public class Memo {
-    private Long id; //PK
-    private String username; //작성자
-    private String contents; //메모 내용
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "username", nullable = false)
+    private String username;
+    @Column(name = "contents", nullable = false, length = 500)
+    private String contents;
 
     public Memo(MemoRequestDto requestDto) {
         this.username = requestDto.getUsername();
--- src/main/java/com/sparta/nbcspringmemo/repository/MemoRepository.java
+++ src/main/java/com/sparta/nbcspringmemo/repository/MemoRepository.java
@@ -3,11 +3,13 @@ package com.sparta.nbcspringmemo.repository;
 import com.sparta.nbcspringmemo.dto.MemoRequestDto;
 import com.sparta.nbcspringmemo.dto.MemoResponseDto;
 import com.sparta.nbcspringmemo.entity.Memo;
+import jakarta.persistence.EntityManager;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -86,5 +88,14 @@ public class MemoRepository {
                 return null;
             }
         }, id);
+    }
+    @Transactional
+    public Memo createMemo(EntityManager em) {
+        Memo memo = em.find(Memo.class, 1);
+        memo.setUsername("Robbie");
+        memo.setContents("@Transactional 전파 테스트 중!");
+
+        System.out.println("createMemo 메서드 종료");
+        return memo;
     }
 }
