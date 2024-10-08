8976e97dd32f6c6ac478c01515ec7f9b4089ee7a
anewsdev <anewsdev@gmail.com>
2024-10-08 11:02:04 +0900

    Query Method란 무엇일까?

--- src/main/java/com/sparta/nbcspringmemo/repository/MemoRepository.java
+++ src/main/java/com/sparta/nbcspringmemo/repository/MemoRepository.java
@@ -4,6 +4,10 @@ import com.sparta.nbcspringmemo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-    
+    List<Memo> findAllByOrderByModifiedAtDesc();
+
+    List<Memo> findAllByUsername(String username);
 }
